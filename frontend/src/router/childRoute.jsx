ec5c204328ff42a9a913d644234ceb75124af734
Md. Irfan Khan <learn.irfankhan@gmail.com>
2025-09-13 22:30:27 +0600

    all product list added

--- frontend/src/router/childRoute.jsx
+++ frontend/src/router/childRoute.jsx
@@ -1,9 +1,14 @@
+import AllProduct from "../components/AllProduct/AllProduct";
 import Home from "../components/Home/Home";
 
 const childRoute=[
     {
         path:'/',
         element:<Home></Home>
+    },
+    {
+        path:'/allproducts',
+        element:<AllProduct></AllProduct>
     }
 ]
 
