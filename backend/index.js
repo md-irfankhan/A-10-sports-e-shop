bd6c9e6ff6e50e9c6195b9f87d0cac4152c3dda3
Md. Irfan Khan <learn.irfankhan@gmail.com>
2025-09-16 06:57:45 +0600

    Delete method added

--- backend/index.js
+++ backend/index.js
@@ -54,6 +54,13 @@ async function run() {
       res.send(result);
       
     })
+    app.delete('/delete/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result=await spProducts.deleteOne(query);
+      res.send(result)
+    })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
