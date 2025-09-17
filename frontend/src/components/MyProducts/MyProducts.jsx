5df721e430113875164ad122cedde8c941107f0e
Md. Irfan Khan <learn.irfankhan@gmail.com>
2025-09-17 14:42:47 +0600

    Product delete added

--- frontend/src/components/MyProducts/MyProducts.jsx
+++ frontend/src/components/MyProducts/MyProducts.jsx
@@ -2,19 +2,55 @@ import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from 
 import { useEffect, useState } from "react";
 import { useAuth } from "../../AuthProvider/AuthProvider";
 import { Link } from "react-router";
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
 
 const MyProducts = () => {
-  const {user}=useAuth()
-  const [myProd,setmyProd]=useState([])
-  useEffect(()=>{
-    fetch(`http://localhost:3000/all/${user?.email}`).then(res=>res.json())
-    .then(data=>{
-      setmyProd(data)
-    })
+  const { user } = useAuth()
+  const [myProd, setmyProd] = useState([])
+  useEffect(() => {
+    fetch(`http://localhost:3000/all/${user?.email}`).then(res => res.json())
+      .then(data => {
+        setmyProd(data)
+      })
 
-  },[])
-    return (
-         <div className="overflow-x-auto">
+  }, [])
+
+  const handleDelete = (id) => {
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/delete/${id}`, {
+          method: 'DELETE'
+        }).then(res => res.json())
+          .then(data => {
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your product has been deleted.",
+                icon: "success"
+              });
+
+              const filtered=myProd.filter(prod=>prod._id!=id)
+              setmyProd(filtered);
+
+            }
+          })
+
+      }
+    });
+
+  }
+  return (
+    <div className="overflow-x-auto">
       <Table hoverable>
         <TableHead>
           <TableRow>
@@ -33,35 +69,35 @@ const MyProducts = () => {
         <TableBody className="divide-y">
 
           {
-            myProd.map(product=>(
-          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              {product.productName}
-            </TableCell>
-            <TableCell>{product.productColor}</TableCell>
-            <TableCell>{product.category}</TableCell>
-            <TableCell>{product.price}</TableCell>
-            <TableCell>
-              <Link to={`/update/${product._id}`} className="font-medium text-primary-600 hover:underline dark:text-primary-500">
-                Edit
-              </Link>
-            </TableCell>
-            <TableCell>
-              <a className="font-medium text-primary-600 hover:underline dark:text-primary-500">
-                Delete
-              </a>
-            </TableCell>
-          </TableRow>
+            myProd.map(product => (
+              <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  {product.productName}
+                </TableCell>
+                <TableCell>{product.productColor}</TableCell>
+                <TableCell>{product.category}</TableCell>
+                <TableCell>{product.price}</TableCell>
+                <TableCell>
+                  <Link to={`/update/${product._id}`} className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                    Edit
+                  </Link>
+                </TableCell>
+                <TableCell>
+                  <a onClick={() => handleDelete(product._id)} className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                    Delete
+                  </a>
+                </TableCell>
+              </TableRow>
             )
 
-          )
+            )
           }
 
-         
+
         </TableBody>
       </Table>
     </div>
-    );
+  );
 };
 
 export default MyProducts;
